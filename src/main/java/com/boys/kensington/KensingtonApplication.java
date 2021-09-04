e040f2c248ce909e952eaba40e4eb7599fb24528
Jake Toan <jake.toan@signifyd.com>
2021-09-04 12:59:17 +0100

    First commit

---
+++ src/main/java/com/boys/kensington/KensingtonApplication.java
@@ -0,0 +1,13 @@
+package com.boys.kensington;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KensingtonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KensingtonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/boys/kensington/KensingtonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boys.kensington;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KensingtonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
