7f9f42af19c6b64780015f2d3c50db331c262287
connormcilwain <74579033+connormcilwain@users.noreply.github.com>
2021-09-04 13:36:17 +0100

    Update BoyController.java

--- src/main/java/com/boys/kensington/controller/BoyController.java
+++ src/main/java/com/boys/kensington/controller/BoyController.java
@@ -1,9 +1,11 @@
 package com.boys.kensington.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/boy")
+@RestController
+@RequestMapping("/boy")
 public class BoyController {
 
 
